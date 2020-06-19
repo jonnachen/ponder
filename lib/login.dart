6fd95666c3752abe59eececb24abd0eec85545a3
Angelina Saliling <ajs649@cornell.edu>
2020-06-19 12:25:34 -0500

    Start setting up login

---
+++ lib/login.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text("sign up"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'homepage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'Login.dart';
 import 'archive.dart';
 import 'account.dart';
+import 'login.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +15,80 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
         title: 'Ponder',
         theme: ThemeData(fontFamily: 'SF Pro'),
-        home: MyBottomNavigationBar());
+        initialRoute: '/',
+        routes: {
+          // When navigating to the "/" route, build the Login widget.
+          '/': (context) => Login(),
+          // When navigating to the "/second" route, build the bottom navigation widget.
+          '/home': (context) => MyBottomNavigationBar(),
+        }
+    );
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login>{
+  bool loggedIn  = false;
+  TextEditingController nameController = TextEditingController();
+
+  @override
+  void initState(){
+    super.initState();
+    autoLogin();
+  }
+
+  void autoLogin() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String user = prefs.getString('user');
+
+    if (user != null){
+      setState(() {
+        loggedIn = true;
+        user;
+      });
+      return;
+    }
+  }
+
+  Future<Null> login(){
+    print("if I actually had this set up, this would log the  user in");
+  }
+
+  Future<Null> logout(){
+    print("if I actually had this set up, this would log the  user out");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Auto Login Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            !loggedIn ? TextField(
+              textAlign: TextAlign.center,
+              controller: nameController,
+              decoration: InputDecoration(
+                  border: InputBorder.none, hintText: 'Please enter your name'),
+            ) : Text('You are logged in'),
+            SizedBox(height: 10.0),
+            RaisedButton(
+              onPressed: () {
+                loggedIn ? logout() : login();                
+              },
+              child: loggedIn ? Text('Logout') : Text('Login'),
+            )
+          ],
+        ),
+      ), 
+    );
   }
 }
 
@@ -25,7 +100,7 @@ class MyBottomNavigationBar extends StatefulWidget {
 class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
   int _currentIndex = 0;
   final List<Widget> _children = [
-    Homepage(),
+    Login(),
     Archive(),
     Account(),
   ];
