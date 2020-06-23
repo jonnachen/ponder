16dc7f31a6614b9af47786d3bafaeb7bf635aba6
juliehelenek <63691646+juliehelenek@users.noreply.github.com>
2020-06-22 23:44:29 -0400

    updated articles page

--- lib/archive.dart
+++ lib/archive.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 
 class ArchiveTab extends StatefulWidget {
   @override
@@ -123,7 +125,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount: 3,
+                                crossAxisCount: 4,
                                 children: <Widget>[
                                   Journal(),
                                   Journal(),
@@ -171,7 +173,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount:3,
+                                crossAxisCount:4,
                                 children: <Widget>[
                                   Journal(),
                                   Journal(),
@@ -212,7 +214,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount:3,
+                                crossAxisCount:4,
                                 children: <Widget>[
                                   Journal(),
                                 ],
@@ -338,7 +340,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount: 3,
+                                crossAxisCount: 4,
                                 children: <Widget>[
                                   Journal(),
                                   Journal(),
@@ -379,7 +381,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount:3,
+                                crossAxisCount:4,
                                 children: <Widget>[
                                   Journal(),
                                   Journal(),
@@ -420,7 +422,7 @@ class _ArchiveTabState extends State<ArchiveTab> {
                                 ),
                               ),
                               child: GridView.count(
-                                crossAxisCount:3,
+                                crossAxisCount:4,
                                 children: <Widget>[
                                   Journal(),
                                 ],
@@ -453,8 +455,9 @@ class __ArticleViewState extends State<_ArticleView> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFFD4FFFF),
-      body: Stack(
-        children: <Widget>[
+      body:
+      //Stack(
+        //children: <Widget>[
           ListView(
             children:<Widget>[
               Padding(
@@ -485,7 +488,7 @@ class __ArticleViewState extends State<_ArticleView> {
                 ),
               ),
               Container(
-                height: 800,
+                height: 1000,
                 child: ListView(
                   children: <Widget>[
                     Padding(
@@ -497,104 +500,191 @@ class __ArticleViewState extends State<_ArticleView> {
                       ),
                     ),
                     Container(
-                      height: 700,
+                      height: 350,
                       decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(30.0),
-                            topRight: Radius.circular(30.0),
-                            bottomLeft: Radius.circular(30.0),
-                            bottomRight: Radius.circular(30.0),
-                          )
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
                       ),
-                      child:Padding(
-                        padding: EdgeInsets.only(top:1.0),
-                        child: Stack(
-                          children: <Widget>[
-                            Positioned(
-                              left:335,
-                              top:10,
-                              child:FlatButton(
-                                onPressed:(){
-                                  setState(()
-                                  {
-                                    isPressed = !isPressed;
-                                  }
-                                  );
-                                },
-                                child: Image(
-                                  width:30,
-                                  height:30,
-                                  image:(isPressed) ? AssetImage('img/star.png') : AssetImage('img/filled_star.png'),
-                                ),
+                      child: Stack(
+                        children: <Widget>[
+                          Positioned(
+                            left: 335,
+                            top:10,
+                            child: FlatButton(
+                              onPressed:(){
+                                setState(() {
+                                  isPressed = !isPressed;
+                                });
+                              },
+                              child: Image(
+                                width: 30,
+                                height: 30,
+                                image: (isPressed) ? AssetImage('img/star.png') : AssetImage('img/filled_star.png'),
                               ),
                             ),
-                            Positioned(
-                              left:100,
-                              top:20,
-                              child: Text('{Date Here}',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 40.0)
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top:75, left:25),
-                              child:Text('{Time Here}',
-                                style: TextStyle(
+                          ),
+                          Positioned(
+                            left: 100,
+                            top: 20,
+                            child: Text("{Date Here}",
+                              style: TextStyle(
                                   color: Colors.black,
-                                  fontWeight:FontWeight.bold,
-                                  fontSize:20,
-                                ),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 40.0
                               ),
                             ),
-                            Padding(
-                              padding: EdgeInsets.only(top:110),
-                              child:Container(
-                                height:350,
-                                width: MediaQuery.of(context).size.width,
-                                decoration:BoxDecoration(
-                                    color: Colors.white,
-                                    boxShadow: [
-                                      BoxShadow(
-                                        color: Colors.grey.withOpacity(0.5),
-                                        spreadRadius: 5,
-                                        blurRadius: 7,
-                                        offset: Offset(0, 3), // changes position of shadow
+                          ),
+                          Positioned(
+                            left:25,
+                            top:75,
+                            child: Text("{Time Here}",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(top:120),
+                            child: Container(
+                              height:200,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  boxShadow:[
+                                    BoxShadow(
+                                      color: Colors.grey.withOpacity(0.5),
+                                      spreadRadius: 5,
+                                      blurRadius:7,
+                                      offset:Offset(0,3),
+                                    ),
+                                  ],
+                                  borderRadius: BorderRadius.all(Radius.circular(30))
+                              ),
+                              child: ListView(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: EdgeInsets.only(top:17,left:17,right:13),
+                                    child: Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minim",
+                                      style: TextStyle(
+                                          height:1.3,
+                                          fontSize:20.0
                                       ),
-                                    ],
-                                    borderRadius: BorderRadius.only(
-                                        topLeft: Radius.circular(30.0),
-                                        topRight: Radius.circular(30.0))
-                                ),
-                                child: Padding(
-                                  padding:EdgeInsets.only(top:17,left:17,right:13),
-                                  child:Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minim",
-                                    style: TextStyle(
-                                        height:1.3,
-                                        fontSize:20.0
                                     ),
                                   ),
-                                ),
+                                ],
                               ),
                             ),
-                          ],
+                          ),
+
+                        ],
+                      ),
+                    ),
+              Stack(
+                children:<Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(top:15),
+                    child:  Center(
+                      child: Text("Based on your journal entry,",
+                        style: TextStyle(
+                          fontSize:25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF525764),
                         ),
                       ),
                     ),
-
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top:40),
+                    child: Center(
+                      child: Text("you should read this:",
+                        style: TextStyle(
+                          fontSize:25,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF525764),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top:20),
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFFAF3DD),
+                  ),
+                  child: Stack(
+                    children: <Widget> [
+                      Positioned(
+                        top: 25,
+                        left: 20,
+                        child: Text("Stress 101",
+                          style: TextStyle(
+                            fontSize:40,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF525764),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top:70,
+                        left:20,
+                        child: InkWell(
+                          child: Text("Open Article",
+                            style: TextStyle(
+                              fontSize:25,
+                              color: Color(0xFF525764),
+                            ),
+                          ),
+                          onTap:() => launch("https://www.google.com"),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+                    Container(
+                      height:300,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow:[
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius:7,
+                              offset:Offset(0,3),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.all(Radius.circular(30))
+                      ),
+                      child: ListView(
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.only(top:17,left:17,right:13),
+                            child: Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,  Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minim",
+                              style: TextStyle(
+                                  height:1.3,
+                                  fontSize:20.0
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
 
-
             ],
           ),
-        ],
-      ),
+        //],
+     // ),
     );
   }
+
 }
 
 class Journal extends StatefulWidget {
